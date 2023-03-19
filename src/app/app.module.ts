17ce1e4c0b0f24f47f6d8a653b8933801e2c3e51
Javier Rodriguez Valdez <javierrv@MacBook-Pro-de-Javier.local>
2023-03-18 23:05:52 -0600

    add item-card component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
 import { ProductListComponent } from './product-list/product-list.component';
+import { ItemCardComponent } from './item-card/item-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ShoppingCartComponent,
     ProductListComponent,
+    ItemCardComponent,
   ],
   imports: [
     BrowserModule,
