c3e0790c523ee8c68f887d36a8b159e3339055c5
Javier Rodriguez Valdez <javierrv@MacBook-Pro-de-Javier.local>
2023-03-19 00:02:38 -0600

    add ngFor on product-list component

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -8,4 +8,12 @@ import { MatDialogRef } from '@angular/material/dialog';
 })
 export class ProductListComponent {
   constructor(public dialogRef: MatDialogRef<ProductListComponent>) {}
+
+  products = [
+    { "name": 'vel1', "description": 'descripción 1' }, 
+    { "name": 'vel2', "description": 'descripción 2' }, 
+    { "name": 'vel3', "description": 'descripción 3' }, 
+    { "name": 'vel4', "description": 'descripción 4' }, 
+    { "name": 'vel5', "description": 'descripción 5' }
+  ];
 }
