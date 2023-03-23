d873a8c3af15f9d579957dacae14019eec257365
Javier Rodriguez Valdez <javierrv@MacBook-Pro-de-Javier.local>
2023-03-22 23:11:23 -0600

    update mock items

--- src/app/items.ts
+++ src/app/items.ts
@@ -23,6 +23,26 @@ export const products: Item[] = [
   {
       title: "vel05",
       description: "desc05"
+  },
+  {
+    title: "per01",
+    description: "desc01"
+  },
+  {
+    title: "per02",
+    description: "desc02"
+  },
+  {
+      title: "per03",
+      description: "desc03"
+  },
+  {
+      title: "per04",
+      description: "desc04"
+  },
+  {
+      title: "per05",
+      description: "desc05"
   }
 ];
   
