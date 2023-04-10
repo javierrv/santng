88ef6340589dd03aa45b554d5dc20b9d3ad950f9
Javier Rodriguez Valdez <javierrv@MacBook-Pro-de-Javier.local>
2023-04-09 22:11:52 -0600

    add quantity attribute to Item type

--- src/app/items.ts
+++ src/app/items.ts
@@ -1,59 +1,70 @@
 export interface Item {
   title: string;
   description: string;
-  price: number
+  price: number,
+  quantity: number
 };
   
 export const products: Item[] = [
   {
     title: "vel01",
     description: "This is the description for vel01",
-    price: 10
+    price: 10,
+    quantity: 1
   },
   {
     title: "vel02",
     description: "This is the description for vel02",
-    price: 20
+    price: 20,
+    quantity: 1
   },
   {
       title: "vel03",
       description: "This is the description for vel03",
-      price: 30
+      price: 30,
+      quantity: 1
   },
   {
       title: "vel04",
       description: "This is the description for vel04",
-      price: 40
+      price: 40,
+      quantity: 1
   },
   {
       title: "vel05",
       description: "This is the description for vel05",
-      price: 50
+      price: 50,
+      quantity: 1
   },
   {
     title: "per01",
     description: "This is the description for per01",
-    price: 1
+    price: 1,
+    quantity: 1
   },
   {
     title: "per02",
     description: "This is the description for per02",
-    price: 2
+    price: 2,
+    quantity: 1
   },
   {
       title: "per03",
       description: "This is the description for per03",
-      price: 3
+      price: 3,
+      quantity: 1
   },
   {
       title: "per04",
       description: "This is the description for per04",
-      price: 4
+      price: 4,
+      quantity: 1
   },
   {
       title: "per05",
       description: "This is the description for per05",
-      price: 5
+      price: 5,
+      quantity: 1
   }
 ];
   
